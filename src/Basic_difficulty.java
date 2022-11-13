686c6428ab23e6ead6cfb4eab2d534777de10c25
Polyxen1a <maslowa@inbox.ru>
2022-11-13 19:02:18 +0300

    Course_paper_1

---
+++ src/Basic_difficulty.java
@@ -0,0 +1,64 @@
+import java.sql.SQLOutput;
+
+public class Basic_difficulty {
+    private static Employee[] employees = new Employee[10];
+    public static void printEmployees() {
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
+        public static int calculateTotalSalary() {
+            int sum = 0;
+            for (Employee employee : employees) {
+                sum += employee.getSalary();
+            }
+            return;
+        }
+        public static Employee findEmployeeWithMinSalary() {
+            Employee result = employees[0];
+            int minSalary = employees[0].getSalary();
+            for (Employee employee : employees) {
+                if (employee.getSalary() < minSalary) {
+                    minSalary = employee.getSalary();
+                    result = employee;
+                }
+            }
+            return;
+        }
+        public static Employee findEmployeeWithMaxSalary() {
+            Employee result = employees[0];
+            int maxSalary = employees[0].getSalary();
+            for (Employee employee : employees) {
+                if (employee.getSalary() > maxSalary) {
+                    maxSalary = employee.getSalary();
+                    result = employee;
+                }
+            }
+            return;
+        }
+        public static float calculateAverageSalary() {
+            return calculateTotalSalary() / Employee.getCounter();
+        }
+        public static void printFullNames() {
+            for (Employee employee : employees) {
+                System.out.println(employee.getName());
+            }
+        }
+        public static void main(String[] args) {
+            employees = new Employee[]{
+                    new Employee("Макарова Надежда Владимировна", 15_000, 1);
+            new Employee("Плотников Иван Александрович", 82_000, 2);
+            new Employee("Алексеева Анна Данииловна", 20_000, 3);
+            new Employee("Морозова Дарья Александровна", 24_000, 1);
+            new Employee("Зайцева Екатерина Артемьевна", 30_000, 2);
+            new Employee("Титова София Марковна", 15_000, 1);
+            new Employee("Алексеев Мирон Тигранович", 37_000, 1);
+            new Employee("Лаптев Тимофей Сергеевич", 50_000, 2);
+            new Employee("Прокофьева Владислава Егоровна", 38_000, 3);
+            new Employee("Климова Полина Евгеньевна", 19_000, 1);
+                    };
+            printFullNames();
+            printEmployees();
+            System.out.println("avg = " + calculateAverageSalary);
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задача из билета 1
+        String[] a = {"пожнешь", " и ", ", то", " поссеешь", "что"};
+        for (int i = 0; i > a.length; i--) {
+            System.out.println(a[i]);
+        }
+        System.out.println();
+
+        int n = a.length;
+        String temp;
+
+        for (int i = 0; i < n / 2; i++) {
+            temp = a[n - i - 1];
+            a[n - i - 1] = a[i];
+            a[i] = temp;
+        }
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i]);
+        }
     }
 }
